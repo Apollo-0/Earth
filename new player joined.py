9d82fbb269ea0f454cb3ba6d30b1820a974d3d49
Apollo-0 <xthecharlieeffectx@gmail.com>
2022-05-25 15:51:31 +0000

    look at me go

--- new player joined.py
+++ new player joined.py
@@ -18,26 +18,44 @@ for line in file:
 file.close()
 
 print()
+#new player joined the lobby
 print(transcript1[0])
 time.sleep(1)
+#welcome player : '?'
 print(transcript1[1])
 time.sleep(2)
 print()
+#hello?
 print(transcript1[2])
 time.sleep(2)
+#is anybody out there?
 print(transcript1[3])
-time.sleep(1)
-print(transcript1[4])
 time.sleep(2)
+#...
+print(transcript1[4])
+time.sleep(3)
+#hello!
 print(transcript1[5])
-time.sleep(1)
+time.sleep(2)
 print()
+#*from behind*
 print(transcript2[0])
 print()
-time.sleep(1)
+time.sleep(2)
+#hello.
 print(transcript2[1])
 print()
+time.sleep(1.5)
+#argg
 print(transcript1[6])
 print()
-time.sleep(1)
+time.sleep(2)
 print(transcript2[2])
+time.sleep(2)
+print()
+#*stares*
+print(transcript2[3])
+print()
+time.sleep(3)
+#why are you yelling
+print(transcript2[4])
